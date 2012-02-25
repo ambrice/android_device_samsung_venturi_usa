22fa22fc3e1eb5cdc2d4059a0841af06397ea11e
Aaron Brice <aaron.brice@datasoft.com>
2012-02-24 17:39:08 -0700

    Initial non-working snapshot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_venturi_usa.mk
+    $(LOCAL_DIR)/full_venturi_usa.mk
---
+++ full_venturi_usa.mk
@@ -0,0 +1,26 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/venturi_usa/venturi_usa.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/venturi_usa/overlay
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/samsung/venturi_usa/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_venturi_usa
+PRODUCT_DEVICE := venturi_usa
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := YP-G70
